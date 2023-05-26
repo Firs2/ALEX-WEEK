4932c23d60f3a9f4ff921912cc0963e6524078ab
Firs2 <canek.m00@mail.ru>
2023-05-26 14:25:52 +0300

    ALEX-WEEK

---
+++ SCRIPTS/WEAPON/ICrosshairPreset.cs
@@ -0,0 +1,11 @@
+﻿namespace FIRS.UIModules.UIElements.Crosshair
+{
+    public interface ICrosshairPreset
+    {
+        /// <summary>
+        /// Draw crosshair elements GUI layout taking into account hide state.
+        /// </summary>
+        /// <param name="spread"></param>
+        void DrawElements(float spread);
+    }
+}
