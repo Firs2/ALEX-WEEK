eae19130a29f176d340792d435913f370dcd622b
Firs2 <canek.m00@mail.ru>
2023-05-26 13:59:25 +0300

    ALEX-WEEK

---
+++ SCRIPTS/AI/CoverPoint.cs
@@ -0,0 +1,64 @@
+﻿using FIRS.Attributes;
+using UnityEngine;
+
+namespace FIRS.AIModules.CoverSystem
+{
+    [System.Serializable]
+    public sealed class CoverPoint
+    {
+        [SerializeField]
+        [NotNull]
+        private Transform point;
+
+        [SerializeField]
+        [Slider(0, 360)]
+        private float angle = 90;
+
+        [SerializeField]
+        private bool isOccupied;
+
+        #region [Aurora Engine Debug Directive]
+#if AURORA_ENGINE_DEBUG && UNITY_EDITOR
+        [SerializeField]
+        internal bool Visualize;
+#endif
+        #endregion
+        public bool IsCover(Transform relative)
+        {
+            Vector3 difference = relative.position - point.position;
+            return Vector3.Angle(point.forward, difference) <= (angle / 2);
+        }
+
+        #region [Getter / Setter]
+        public Transform GetPoint()
+        {
+            return point;
+        }
+
+        public void SetPoint(Transform value)
+        {
+            point = value;
+        }
+
+        public float GetForwardTolerance()
+        {
+            return angle;
+        }
+
+        public void SetForwardTolerance(float value)
+        {
+            angle = value;
+        }
+
+        public bool IsOccupied()
+        {
+            return isOccupied;
+        }
+
+        public void IsOccupied(bool value)
+        {
+            isOccupied = value;
+        }
+        #endregion
+    }
+}
